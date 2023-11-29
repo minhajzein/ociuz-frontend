f9f78f209899c4760ba83b0a84a80ad14ea39241
minhajzein <109206665+minhajzein@users.noreply.github.com>
2023-11-29 18:04:18 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div className="justify-center items-center w-full">
+				<p className="text-center text-yellow-500">Hello World</p>
+			</div>
+		</>
+	);
+}
+
+export default App;
